841e9c16129edcf73a88ac755a91d3177faeaaa3
=Raunak <=sharmaraunak338@gmail.com>
2019-05-07 07:41:24 +0530

    library management

---
+++ README.md
@@ -0,0 +1,46 @@
+# Project Title
+
+This is a library mangement system made in core php, the following technologies were used:
+
+- HTML
+- CSS
+- Bootstrap
+- PHP
+- JS
+- SQL
+
+### Prerequisites
+
+- XAMPP (if localhost)
+
+- Any Server
+
+You would also need SQL to manage backend.
+
+### Installing
+
+Download and run on server.
+
+## Functions:
+
+The project has two interfaces in general
+
+1. User interface
+
+2. Admin interface
+
+3. Login interface for both above.
+
+### User Interface:
+
+The user interface deals with
+
+- Displaying available books
+
+- Searching for books
+
+- Issue Books (it is ensured that the book is already not issued).
+
+- Reserve Books (it is ensured that the book is not reserved before, and to let the user reserve it on a future date).
+
+- Search books ( based on query entered the book is searched).
---
+++ login.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="./style.css"/>
+</head>
+<body>
+<div class = "container">
+	<div class="wrapper">
+		<form action="login.php" method="post" name="Login_Form" class="form-signin">       
+		    <h3 class="form-signin-heading">Welcome Back! Please Sign In</h3>
+			  <hr class="colorgraph"><br>
+			  
+			  <input type="text" class="form-control" name="Username" placeholder="Username" required="" autofocus="" />
+			  <input type="password" class="form-control" name="Password" placeholder="Password" required=""/>     		  
+			 
+			  <button class="btn btn-lg btn-primary btn-block"  name="Submit" value="Login" type="Submit">Login</button>  			
+		</form>		
+		<?php
+			if(isset($_POST['Submit'])){
+				$username = $_POST['Username'];
+				$password = $_POST['Password'];	
+				$con = mysqli_connect('localhost','root','','library');
+				if($con){
+					$query = "select * from admin_login where 
+					admin_name='$username' AND admin_pass='$password'";
+					echo "<script>alert($username,$password);</script>"
+					
+					$result = mysqli_query($con,$query);
+					if(mysqli_num_rows($result)>0){
+						$_SESSION['user'] = $username;
+						$_SESSION['pass'] = $password;
+						echo "<script>
+							alert('You are authenticated!');
+							window.location.href='home.php';
+						</script>";
+
+					}
+					else{
+						echo "<script>
+							alert('You are not authenticated!');
+							</script>";
+					}
+				}
+
+			}
+			
+
+		?>	
+	</div>
+</div>
+</body>
